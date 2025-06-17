eb82f2444b60941c315527fab39dd8247248b002
Shae-Lynn Hendrickse <shaelynnsampson4@gmail.com>
2025-06-17 14:05:14 +0200

    updated the RLI function

--- scripts/RLI_graphs_fun.R
+++ scripts/RLI_graphs_fun.R
@@ -6,37 +6,52 @@
 ## Author: Shae-Lynn Hendricks
 ##
 ## Date Created: 2024-07-24
+## Date updated: 2025-06-17
 ##
-## Notes: Dataframe only requires the Red List Index (RLI), year, minimum and
-##        maximum values for each year. I am not sure what the raw data looks like,
-##        I just used the tables from the excel figures Andrew shared.
+## Notes: Dataframe requires the Red List Index (RLI), year, minimum and
+##        maximum values for each year and potentially the group for categorising the plot.
 ##
 #####################################################################################
 ### Load libraries
 
+library(dplyr)
 library(ggplot2)
+library(rlang)
 
-RLI_graph <- function(DF,YEAR, RLI, min, max, GRP = FALSE){
+RLI_plot <- function(DF, YEAR, RLI, min, max, GROUP = NULL, summarise_by_year = TRUE) {
+  # Convert column names to symbols
+  YEAR <- enquo(YEAR)
+  RLI <- enquo(RLI)
+  min <- enquo(min)
+  max <- enquo(max)
+  GROUP <- enquo(GROUP)
 
-  if(GRP == TRUE){
-
-    ggplot2::ggplot(DF, aes(x = {{YEAR}}, y = {{RLI}}, group = {{GROUP}}, color = {{GROUP}})) +
-      ggplot2::geom_line(linetype="dashed") +
-      ggplot2::geom_ribbon(aes(ymin = {{min}}, ymax = {{max}}), fill = "grey", alpha = .2, colour = NA)+
-      ggplot2::theme_classic()+
-      ggplot2::ylim(0.7,1)
-
+  # Optional summarisation
+  if (summarise_by_year && rlang::quo_is_null(GROUP)) {
+    DF <- DF %>%
+      group_by(!!YEAR) %>%
+      summarise(
+        !!min := mean(!!min, na.rm = TRUE),
+        !!max := mean(!!max, na.rm = TRUE),
+        !!RLI := mean(!!RLI, na.rm = TRUE),
+        .groups = "drop"
+      )
   }
-  else {
 
-  ggplot2::ggplot(DF, aes(x = {{YEAR}}, y = {{RLI}})) +
-    ggplot2::geom_line(aes(y = {{RLI}})) +
-    ggplot2::geom_ribbon(aes(ymin = {{min}}, ymax = {{max}}),alpha = .3, colour = NA)+
-    ggplot2::theme_classic()+
-    ggplot2::ylim(0.7,1)
+  # Plotting
+  p <- ggplot(DF, aes(x = !!YEAR, y = !!RLI))
 
-
+  if (rlang::quo_is_null(GROUP)) {
+    p <- p +
+      geom_line() +
+      geom_ribbon(aes(ymin = !!min, ymax = !!max), alpha = 0.3, colour = NA)
+  } else {
+    p <- p +
+      geom_line(aes(group = !!GROUP, color = !!GROUP), linetype = "dashed") +
+      geom_ribbon(aes(ymin = !!min, ymax = !!max, group = !!GROUP), fill = "grey", alpha = 0.2, colour = NA)
   }
+
+  p + theme_classic() + ylim(0.7, 1)
 }
 
 
